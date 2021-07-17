115a84825c62e5f314c23ea889c24851e94c0adc
SameThingHi <87526869+SameThingHi@users.noreply.github.com>
2021-07-17 18:44:16 -0300

    Update app.js
    Adding events

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 "use strict"
 
-var saludo = "Bienvenido";
+//var saludo = "Bienvenido";
 
-console.log(saludo);
+//console.log(saludo);
 
-alert("Ésta es una alerta con Javascript, acepta para continuar a la web")
+//alert("Ésta es una alerta con Javascript, acepta para continuar a la web")
 
+const botones = document.querySelector(".botones");
+
+botones.addEventListener('click', function () {
+    console.log("El boton se ha pulsado");
+})
+
+botones.addEventListener('mouseover', function () {
+    console.log("El mouse esta sobre el botón");
+})
+
+botones.addEventListener('mouseout', function () {
+    console.log("El mouse esta fuera del botón");
+})
